dca05af02e823e52a901a7a340ff54336136c86c
Jerome Alexander Graves <YungThunder@users.noreply.github.com>
2018-12-19 17:27:02 +0000

    Implemented simple LCD. Shows the encoder values and title.

--- ArduinoSPI.ino
+++ ArduinoSPI.ino
@@ -13,35 +13,47 @@
 
 #include <SPI.h>
 #include <EnableInterrupt.h>
+#include <LiquidCrystal.h>
 
 // Pins for encoders.
 #define ENCODER0_PIN_A  A8
 #define ENCODER0_PIN_B  A9
-#define ENCODER1_PIN_A  A10 
-#define ENCODER1_PIN_B  A11 
+#define ENCODER1_PIN_A  A10
+#define ENCODER1_PIN_B  A11
 
+//Pins for LCD.
+#define LCD_RS          12
+#define LCD_EN          11
+#define LCD_D4          5
+#define LCD_D5          4
+#define LCD_D6          3
+#define LCD_D7          2
 
-// SPI slave select pin for FPGA
+// SPI slave select pin for FPGA.
 #define SLAVESELECT_PIN 10
 
 
-// Position stored as byte.
+// Encoder position stored as byte.
 volatile byte encoder0Pos = 0 ;
 volatile byte encoder1Pos = 0 ;
 
-
+// Create LCD object.
+LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 
 void setup() {
   Serial.begin(9600);
 
+  // Set up the LCD's number of columns and rows.
+    lcd.begin(16, 2);
 
-  // Set the slaveSelectPin as an output:
+
+  // Set the slaveSelectPin as an output.
   pinMode(SLAVESELECT_PIN, OUTPUT);
   // initialize SPI:
   SPI.begin();
 
 
-   // Set Encoder pins to Input and enable Pullup resistors.
+  // Set Encoder pins to Input and enable Pullup resistors.
   pinMode(ENCODER0_PIN_A, INPUT_PULLUP);
   pinMode(ENCODER0_PIN_B, INPUT_PULLUP);
   pinMode(ENCODER1_PIN_A, INPUT_PULLUP);
@@ -53,17 +65,19 @@ void setup() {
 }
 
 void loop() {
+  delay(100); // debug delay
+
+  // Send over SPI.
   SPI_send(encoder0Pos);
   SPI_send(encoder1Pos);
-  delay(1000);
-  disableInterrupt(ENCODER0_PIN_A);
-  disableInterrupt(ENCODER1_PIN_A);
-  Serial.print("--- sent :");
-  Serial.print(encoder0Pos, DEC);
-  Serial.print(", ");
-  Serial.print(encoder1Pos, DEC);
-  Serial.print(" --- ");
-  enableInterrupt(ENCODER0_PIN_A, encoder0_Interrupt, FALLING);
-  enableInterrupt(ENCODER1_PIN_A, encoder1_Interrupt, FALLING);
-  
+
+  // Print to LCD.
+  lcd.clear();
+  lcd.print(" !!Synthy Boy!!");
+  lcd.setCursor(0, 1);
+  lcd.print(encoder0Pos, DEC);
+  lcd.setCursor(7, 1);
+  lcd.print("::");
+  lcd.setCursor(13, 1);
+  lcd.print(encoder1Pos, DEC);
 }
